c028940dcfd0ea6aadad9917cafd0a5d8b231176
Nujood Almossa <87212152+nujooda@users.noreply.github.com>
2023-10-05 21:02:46 -0700

    completed

--- CSFundamentals/C01ProgramStructure/Program.cs
+++ CSFundamentals/C01ProgramStructure/Program.cs
@@ -4,25 +4,9 @@
     {
         static void Main(string[] args)
         {
-            // read code at the X location
+            
+            
 
-            string student = "Jessica";
-            string studentID = "G1280";
-            Console.WriteLine(studentID);
-
-            if(student == "Jessica")
-            {
-                int age = 21;
-                Console.WriteLine($"Hello {student}");
-                Console.WriteLine (age);
-            }
-            else 
-            {
-                Console.WriteLine("Hi there...");
-                Console.WriteLine(age);
-            }
-
-            Console.WriteLine(age);
         }
     }
 }
