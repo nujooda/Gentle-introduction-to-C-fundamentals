e19e5c58b968f43d7ee0f22279ee998087cf5d05
Nujood Almossa <87212152+nujooda@users.noreply.github.com>
2023-10-10 22:09:30 -0700

    test

--- CSFundamentals/C02BasicOutput/Program.cs
+++ CSFundamentals/C02BasicOutput/Program.cs
@@ -4,12 +4,8 @@
     {
         static void Main(string[] args)
         {
-            // 1. data type
-            // 2. Name the variable
-            string studentName; // Variable declaration
-            studentName = "Nujood"; // Variable initialization
-
-            double salesDiscount = 0.1;
+            // check notes
+            //Start on Data Types
 
         }
     }
