51ded3fa18d4cfe442ea1c7013f185db917187bb
Nujood Almossa <87212152+nujooda@users.noreply.github.com>
2023-10-14 22:30:18 -0700

    completed

--- CSFundamentals/C02BasicOutput/Program.cs
+++ CSFundamentals/C02BasicOutput/Program.cs
@@ -4,8 +4,72 @@
     {
         static void Main(string[] args)
         {
-            // check notes
-            //Start on Data Types
+            /*char alphabet = 'A';
+            char number = '3';
+            char specialCharacter = '@';*/
+
+            //string name = "Nujood";
+            //string location = "California";
+
+            //string number1 = "1";
+            //string number2 = "2";
+
+            /*double numDouble = 10.5;
+            double numbrDouble = 10.5d;
+
+            Console.WriteLine($"without 'd' suffix: {numDouble}");
+            Console.Write($"without 'd' suffix: {numbrDouble}" );
+
+            Console.WriteLine();
+            var num1 = 3;
+            var num2 = 3d;
+            //var num2 = 3.0;
+
+            Console.Write($"without 'd': {num1/2} ");
+            Console.WriteLine($"with 'd': {num2/2}");
+
+            //float data type
+            float numFloat = 3.5f;
+
+            // decimal data type
+
+            decimal numDecimal = 10.5M;*/
+
+            /*int numberInt = 10;
+            long numberLong = numberInt;
+            double numberDouble = numberInt;
+            decimal numberDecimal = numberInt;
+
+            Console.WriteLine($"Int : {numberInt}");
+            Console.WriteLine($"Long: {numberLong}");
+            Console.WriteLine($"Double: {numberDouble}");
+            Console.WriteLine($"Decimal {numberDecimal}");
+            Console.WriteLine();
+            Console.WriteLine(numberInt.GetType());
+            Console.WriteLine(numberLong.GetType());    
+            Console.WriteLine(numberDouble.GetType());
+            Console.WriteLine(numberDecimal.GetType());
+
+            Console.WriteLine();
+
+            Console.WriteLine(numberDouble/3);
+
+            decimal numDecimal = 10m;
+            double numDouble = (double)numDecimal;
+            long numLong = (long)numDecimal;
+            int numInt = (int)numDecimal;*/
+
+            //convert Class
+
+            string nbrString = "27";
+
+            int nbrInt = Convert.ToInt32(nbrString);
+
+
+
+            Console.ReadKey();
+
+
 
         }
     }
